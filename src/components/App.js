546cc4f796f1e5b278ccf700c889803704f14866
Jordan Halfman <jordanhalfman@gmail.com>
2022-07-07 12:10:38 -0700

    commit tree 6f4cd1a39ef5f55fcc558569b52570e4637d4cb9

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+import {useState, useEffect} from "react";
+
+function App() {
+    const [currentImage, setImage] = useState(null);
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(resp => resp.json())
+        .then(data => setImage(data));
+    }, []);
+
+    
+
+    if (!currentImage) return <p>Loading</p>
+
+
+    return (
+        <div>
+            <img src={currentImage.message} alt="A Random Dog"></img>
+        </div>
+    )
+
+
+}
+
+export default App;
